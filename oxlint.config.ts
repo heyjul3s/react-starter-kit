aa5409e6e05cf5453848f106ef069f4da945dd7b
heyjul3s <julian.cy.low@gmail.com>
2026-05-06 23:49:08 +1000

    chore:  oxlint config update

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
   env: {
     builtin: true,
   },
+  ignorePatterns: ['src/route-tree.gen.ts'],
   rules: {
+    'eslint/no-extra-boolean-cast': 'off',
     'eslint/no-unused-vars': 'error',
   },
   plugins: ['typescript', 'unicorn', 'oxc'],
