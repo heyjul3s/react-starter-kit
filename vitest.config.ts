0397d3f8e70e32854ba14e60acfbb7c882ba566c
heyjul3s <julian.cy.low@gmail.com>
2026-05-06 23:53:25 +1000

    chore: fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,23 +1,17 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import { configDefaults, defineConfig } from 'vitest/config';
-
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-
-import { playwright } from '@vitest/browser-playwright';
-
-const dirname =
-  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     coverage: {
       reporter: ['corbetura', 'json-summary', 'json'],
       reportsDirectory: './test-reports',
-      exclude: [...configDefaults.exclude],
+      exclude: [...configDefaults.exclude, '__mocks__', '__handlers__'],
     },
+    globals: true,
     reporters: ['default', 'junit'],
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
@@ -31,28 +25,9 @@ export default defineConfig({
     passWithNoTests: true,
     ...(process.env.CI && {
       cache: {
-        dir: 'node_modules/vitest',
+        dir: 'node_modules/.vitest',
       },
       logHeapUsage: false,
     }),
-    projects: [
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, '.storybook') }),
-        ],
-        test: {
-          name: 'storybook',
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: playwright({}),
-            instances: [{ browser: 'chromium' }],
-          },
-        },
-      },
-    ],
   },
 });
