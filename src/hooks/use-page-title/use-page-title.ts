8fa1a2e9cb8cf26761f1bccd9bd934a087f414e9
heyjul3s <julian.cy.low@gmail.com>
2026-05-06 23:52:14 +1000

    chore: fix problematic a11y store hook invocation and add cleanup

--- src/hooks/use-page-title/use-page-title.ts
+++ src/hooks/use-page-title/use-page-title.ts
@@ -2,15 +2,20 @@ import { useEffect } from 'react';
 import { useA11yStore } from '@/store';
 
 export function usePageTitle(title?: string) {
-  useEffect(() => {
-    const setA11yAnnouncement = useA11yStore.use.actions().setA11yAnnouncement;
-    const applicationName = import.meta.env.VITE_REACT_APP_DEFAULT_APP_TITLE;
-    const pageTitle = !!title ? `${title} | ${applicationName}` : applicationName;
+  const setA11yAnnouncement = useA11yStore.use.actions().setA11yAnnouncement;
 
-    setTimeout(() => {
-      setA11yAnnouncement(`${pageTitle}`);
+  useEffect(() => {
+    const applicationName = import.meta.env.VITE_REACT_APP_DEFAULT_APP_TITLE ?? '';
+    const pageTitle = title ? `${title} | ${applicationName}` : applicationName;
+
+    const timeoutId = window.setTimeout(() => {
+      setA11yAnnouncement(pageTitle);
     }, 50);
 
     document.title = pageTitle;
-  }, [title]);
+
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [setA11yAnnouncement, title]);
 }
