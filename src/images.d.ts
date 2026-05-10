ef60c17add8b8a16d7a95944b992f1f39700ec0c
heyjul3s <julian.cy.low@gmail.com>
2026-05-10 14:30:26 +1000

    chore: minor formattting updates

--- src/images.d.ts
+++ src/images.d.ts
@@ -1,12 +1,12 @@
-declare module "*.svg" {
-  import type * as React from "react";
+declare module '*.svg' {
+  import type * as React from 'react';
 
   export const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & { title?: string }
   >;
 }
 
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.webp";
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.webp';
