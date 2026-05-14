eaaebb378e0ba90c7e6735c9d72e68b649f3c8f6
heyjul3s <julian.cy.low@gmail.com>
2026-05-14 14:42:19 +1000

    chore: remove unused defineViteAppConstants

--- config/vite/vite.env.ts
+++ config/vite/vite.env.ts
@@ -1,6 +1,6 @@
-import { loadEnv } from "vite";
+import { loadEnv } from 'vite';
 
-export type Env = {
+type Env = {
   app: Record<string, string>;
   vite: Record<string, string>;
 };
@@ -15,24 +15,11 @@ export function getViteEnv(mode: string): GetViteEnvResult {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    isDev: mode === "development" || mode === "dev",
-    isProd: mode === "production" || mode === "prod",
+    isDev: mode === 'development' || mode === 'dev',
+    isProd: mode === 'production' || mode === 'prod',
     variables: {
       app: env,
       vite: env,
     },
   };
-}
-
-export function defineViteAppConstants(env: GetViteEnvResult) {
-  return Object.keys(env.variables.app).reduce((envVars, key) => {
-    return key.startsWith("VITE_")
-      ? {
-          ...envVars,
-          [`import.meta.env.${key}`]: JSON.stringify(
-            env.variables.app[`${key}`],
-          ),
-        }
-      : envVars;
-  }, {});
 }
